97b6b5a2c2297e8017fd4a5fa1d8145119751bcc
Ronald <rjskinnerc@unal.edu.co>
2022-05-11 11:52:32 -0500

    Refactor scores list color schema

--- src/modules/render.js
+++ src/modules/render.js
@@ -1,8 +1,8 @@
 const createScoreNode = (score) => {
   const scoreNode = document.createElement('li');
-  scoreNode.classList.add('odd:text-zinc-200', 'even:text-zinc-400', 'text-base', 'px-3', 'py-2', 'font-semibold', 'tracking-wider', 'flex', 'justify-between')
+  scoreNode.classList.add('odd:text-sky-500', 'even:text-sky-200', 'text-lg', 'px-3', 'py-2', 'font-semibold', 'tracking-wider', 'flex', 'justify-between')
   scoreNode.innerHTML = `
-    ${score.user} <span class="text-sky-100 bg-sky-500 py-0.5 px-2 rounded-lg">${score.score}</span>
+    ${score.user} <span class="text-rose-100 text-base bg-rose-500 py-0.5 px-2 rounded-lg">${score.score}</span>
   `;
 
   return scoreNode;
