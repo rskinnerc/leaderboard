1aa2d462ada279e2ca8080006205088a046b5e45
Ronald <rjskinnerc@unal.edu.co>
2022-05-11 11:26:00 -0500

    Stryle the form container

--- src/modules/render.js
+++ src/modules/render.js
@@ -1,6 +1,6 @@
 const createScoreNode = (score) => {
   const scoreNode = document.createElement('li');
-  scoreNode.classList.add('odd:text-zinc-200', 'even:text-zinc-400', 'text-base', 'px-3', 'py-2', 'font-semibold', 'tracking-wider')
+  scoreNode.classList.add('odd:text-zinc-200', 'even:text-zinc-400', 'text-base', 'px-3', 'py-2', 'font-semibold', 'tracking-wider', 'flex', 'justify-between')
   scoreNode.innerHTML = `
     ${score.user} <span class="text-sky-100 bg-sky-500 py-0.5 px-2 rounded-lg">${score.score}</span>
   `;
